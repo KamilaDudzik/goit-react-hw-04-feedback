c9ac8e1bcbf46f3f0da12ce3ad9861f5438eb3d7
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-10-01 13:57:33 +0200

    Clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { Statistics } from "./Statistics/Statistics";
 import { Section } from "./Section/Section";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Notification } from "./Notification/Notification";
-// import { Component } from "react";
 import { useState } from "react";
  
 export const App = () => {
@@ -24,52 +23,17 @@ export const App = () => {
 
   const countTotalFeedback = () => {
 
-    // return this.state.good + this.state.neutral + this.state.bad;
     return good + neutral + bad;
 
   };
 
   const countPositiveFeedbackPercentage = () => {
-
-    // if (this.countTotalFeedback() === 0) return 0;
-    // return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
-        if (countTotalFeedback() === 0) return 0;
+    
+    if (countTotalFeedback() === 0) return 0;
     return Math.floor((good / countTotalFeedback()) * 100);
 
   };
 
-  // changeState = name => {
-
-  //   this.setState(prevState => ({
-  //     [name]: prevState[name] + 1,
-
-  //   }));
-  // };
-
-  // render() {
-    // return (
-
-    //   <Section title={"Please leave feedback"}>
-
-    //     <FeedbackOptions
-    //       options={Object.keys(feedback)}
-    //       onLeaveFeedback={this.changeState}
-    //     />
-
-    //     {this.countTotalFeedback() ? (
-    //       <Statistics
-    //         good={this.state.good}
-    //         neutral={this.state.neutral}
-    //         bad={this.state.bad}
-    //         total={this.countTotalFeedback()}
-    //         positivePercentage={this.countPositiveFeedbackPercentage()}
-    //       />
-    //     ) : (
-    //       <Notification message="There is no feedback" />
-    //     )}
-
-//   </Section>
-  
   return (
 
       <Section title={"Please leave feedback"}>
@@ -92,10 +56,8 @@ export const App = () => {
         )}
 
       </Section>
-
-
     );
   }
-// }
+
 
 export default App;
